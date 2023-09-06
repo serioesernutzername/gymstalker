46641a6db40262e4019a2d7e7142914d30fcc1ad
inwerk <142039445+inwerk@users.noreply.github.com>
2023-09-06 13:38:36 +0000

    Update gymstalker.py
    Reworked to use GET-request

--- gymstalker.py
+++ gymstalker.py
@@ -3,7 +3,6 @@ import datetime
 import re
 import requests
 
-
 def save_data():
     # dt_now stores current time
     dt_now = datetime.datetime.now()
@@ -11,29 +10,24 @@ def save_data():
 
     # convert datetime to string
     dt_now_str = dt_now.strftime("%m/%d/%Y, %H:%M:%S")
-
-    # create regex for gym occupation
-    regex = r'[0-9]+[&][n][b][s][p][;][B][e][s][u][c][h][e][r]'
-
-    # search Page.html with regex and get result
-    with open('Page.html', 'r') as infile:
-        html_page = infile.read()
-        results = re.findall(regex, html_page)
-        result = str(results[0])
-        gym_occupation = re.search('[0-9]*', result).group()
-
+    
     # append date time string and gym occupation to csvfile
     with open('data.csv', 'a', newline='') as csvfile:
         datawriter = csv.writer(csvfile, delimiter=',')
-        datawriter.writerow([dt_now_str] + [gym_occupation])
+        datawriter.writerow([dt_now_str] + [obtain_data()])
 
 
 def obtain_data():
     url = "https://www.mysports.com/nox/public/v1/studios/1210009740/utilization/v2/active-checkin"
+    
     headers = {
+        "Content-Type": "application/json",
         "x-tenant": "sportfabrik",
         "DNT": "1",
     }
 
-    result = requests.get(url, headers=headers)
-    return result
+    response = requests.get(url, headers=headers)
+    
+    return response.json()['value']
+    
+save_data()
