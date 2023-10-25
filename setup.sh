e20a5083b1eee07c267ece378f1d60417516afc2
inwerk <142039445+inwerk@users.noreply.github.com>
2023-10-25 13:38:20 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/serioesernutzername/gymstalker.git
-pip install -r ./gymstalker/requirements.txt
-python ./gymstalker/webscraper/main.py
+cd ./gymstalker/
+pip install -r ./requirements.txt
+python ./webscraper/main.py
